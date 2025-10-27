4ac62f6f7289b9fb01c2fe7488162ce181a535c1
MatiasCuello01 <cuellomaty01@gmail.com>
2025-10-27 16:39:52 -0300

    index

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'componentes/conexion.php';
-$paquetes = $conexion->query("SELECT * FROM paquete WHERE paquete.disponible=1;");
+$paquetes = $conexion->query("SELECT * FROM paquete WHERE paquete.estado=activo;");
 ?>
 
 <!DOCTYPE html>
