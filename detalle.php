0d5666a68b6ba00b6582dec0a4f753e44ad3476a
benjaaag <benjagomezz051007@gmail.com>
2025-11-03 17:00:22 -0300

    fechas

--- detalle.php
+++ detalle.php
@@ -21,14 +21,14 @@ if($id_paquete != null && $id_paquete > 0) {
     } else{
         $servicio = $conexion->query(query: "
         SELECT * 
-        FROM servicio JOIN paquete_servicio ON servicio.id_servicio = paquete_sevicio.id_servicio
+        FROM servicio JOIN paquete_servicio ON servicio.id_servicio = paquete_sevicio.id_paquete
         WHERE paquete_servicio.id_paquete = $id_paquete; "); 
     }
-    $fechaLimite =  date_create(datetime: $paquete['f_limite']);
+    $fechaLimite =  date_create(datetime: $paquete['fecha_fin']);
     $hoy = new DateTime();
     $dias_restantes = $hoy->diff(targetObject: $fechaLimite)->format(format: '%a');
 
-    $cupo_disponible = $paquete['cupo_total'] - $paquete['cupo_reservado'];
+    $cupo_disponible = $paquete['limite_personas'] - $paquete['cupo_reservado'];
     
 }else {
     echo "<div class='alert-danger'>ID de paquete invalido.</div>";
@@ -39,4 +39,4 @@ if($id_paquete != null && $id_paquete > 0) {
 <main class="flex-shrink-0 py-4">
     <div class="container d-flex justify-content-center">
         <div class="card text-white shadow-lg border-0 rounded-4 overflow-hidden w-100" style="max-width: 800px">
-    
+    <h1>JAJAAJAJJ pudiste abrirlo</h1>
